64aa8b0bc899abda20df19efe044d0514adb83d5
Maryajitha <43032764+Maryajitha@users.noreply.github.com>
2018-09-07 10:55:01 +0530

    Add files via upload

---
+++ arithmetic_operations.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a=2,b=4,c;
+    c=a+b;
+    System.out.println("addition of two numbers"+c);
+    c=a-b;
+    System.out.println("substraction of two numbers"+c);
+    c=a*b;
+    System.out.println("multiplication of two numbers"+c);
+    c=a/b;
+    System.out.println("division of two numbers"+c);
+  }
+}
