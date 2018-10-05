f1825b5eacf2858cbe0a95cea5580f4302caf7a6
Maryajitha <43032764+Maryajitha@users.noreply.github.com>
2018-10-05 12:29:09 +0530

    Add files via upload

---
+++ Kinteger.java
@@ -0,0 +1,17 @@
+import java.io.*; 
+  class Main { 
+    static int findSum(int N, int K) 
+    { 
+        int ans = 0;  
+        int y = N / K; 
+        int x = N % K; 
+        ans = (K * (K - 1) / 2) * y + (x * (x + 1)) / 2; 
+  
+        return ans; 
+    } 
+        static public void main(String[] args) 
+    { 
+        int N = 10, K = 2; 
+        System.out.println(findSum(N, K)); 
+    } 
+} 
